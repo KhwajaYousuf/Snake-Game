9029a85aa80eafa5c153f0f23d58420cfb3b80fb
Yousuf <kyousufmo17@gmail.com>
2024-02-26 17:23:43 -0500

    Added initial skeleton code for Snake Game project

---
+++ GamePanel.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+    
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+}
