3d771fc1123aad6326a3e48b0c1bb46d2609686f
Yousuf <kyousufmo17@gmail.com>
2024-02-26 17:35:06 -0500

    Implemented constructors for GameFrame and GamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -3,6 +3,10 @@ import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() { //constructor for GamePanel
+
+    }
     
     public void actionPerformed(ActionEvent e) {
         
