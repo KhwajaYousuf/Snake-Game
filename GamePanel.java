2d9692019930adf7a24cfeac3abdd2278c60dc0e
yousuff <kyousufmo17@gmail.com>
2024-02-27 18:57:03 -0500

    Implemented constructor setup in GamePanel class

--- GamePanel.java
+++ GamePanel.java
@@ -28,7 +28,11 @@ public class GamePanel extends JPanel implements ActionListener {
 
 
     GamePanel() { //constructor for GamePanel
-
+            random = new Random();
+            this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+            this.setBackground(Color.black);
+            this.setFocusable(true);
+            this.addKeyListener(new MyKeyAdapter());
     }
 
     public void startGame() {
