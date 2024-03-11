ca603c53c4af3cec61e0d18e4fc1bc8d66dc68f7
Yousuf <kyousufmo17@gmail.com>
2024-03-11 18:44:24 -0400

    Added additonal cases

--- GamePanel.java
+++ GamePanel.java
@@ -70,9 +70,15 @@ public class GamePanel extends JPanel implements ActionListener {
         }
 
         switch(direction) {
-        case 'u':
+        case 'U':
             y[0] = y[0] - UNIT_SIZE;
-            break;    
+            break;  
+        case 'D':
+            y[0] = y[0] - UNIT_SIZE;
+            break; 
+        case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;   
         } 
     }
     
