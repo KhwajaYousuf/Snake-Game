461e6345248e96bda680518786efa4336ebb7bd4
Yousuf <kyousufmo17@gmail.com>
2024-03-16 20:21:42 -0400

    Added handling for up arrow key in MyKeyAdapter class

--- GamePanel.java
+++ GamePanel.java
@@ -154,6 +154,11 @@ public class GamePanel extends JPanel implements ActionListener {
                         direction = 'R';
                     }
                     break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
             }
         }
     }
