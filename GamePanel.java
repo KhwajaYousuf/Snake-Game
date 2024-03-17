4c9d43ff1bca2170028f4bc420094e368dcdf38a
Yousuf <kyousufmo17@gmail.com>
2024-03-17 05:48:10 -0400

    Incremented bodyparts and appleseaten aand called newApple method.

--- GamePanel.java
+++ GamePanel.java
@@ -97,8 +97,10 @@ public class GamePanel extends JPanel implements ActionListener {
     
     public void checkApple() {
         if((x[0] == appleX) && y[0] == appleY) {
-            
-        }
+            bodyParts++;
+            applesEaten++;
+            newApple();;
+        }   
     }
 
     public void checkCollisions() {
